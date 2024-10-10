926ddd6afc288edaed324c78b052d504ee2abbfa
Jonáš Šindelář <jonassindelar@seznam.cz>
2024-10-10 19:39:07 +0200

    PRG Metody 3
    C#

---
+++ PRG_METODY3/p01/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace p01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.button = new System.Windows.Forms.Button();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(71, 55);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(184, 22);
+            this.textBox.TabIndex = 0;
+            this.textBox.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+            // 
+            // button
+            // 
+            this.button.Enabled = false;
+            this.button.Location = new System.Drawing.Point(276, 55);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(132, 64);
+            this.button.TabIndex = 1;
+            this.button.Text = "Smazat čísla, rozdělit slova a vypsat do listboxu";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.ItemHeight = 16;
+            this.listBox.Location = new System.Drawing.Point(71, 102);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(184, 276);
+            this.listBox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(68, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Zadejte řetězec:";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(902, 440);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.textBox);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ PRG_METODY3/p01/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace p01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Smaz(ref string text)
+        {
+            string slova = "";
+            foreach (char pismeno in text)
+            {
+                if (!char.IsDigit(pismeno))
+                {
+                    slova += pismeno;
+                }
+            }
+            text = slova;
+        }
+
+        private void Mezery(ref string text)
+        {
+            string slova = "";
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] != ' ' || (slova.Length > 0 && slova[slova.Length - 1] != ' '))
+                {
+                    slova += text[i];
+                }
+            }
+            text = slova;
+        }
+
+        private void Zobraz(string text, ListBox listbox)
+        {
+            listbox.Items.Clear();
+            Mezery(ref text);
+            foreach (string slovo in text.Split(' '))
+            {
+                listbox.Items.Add(slovo);
+            }
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            string text = textBox.Text;
+            Smaz(ref text);
+            Zobraz(text, listBox);
+        }
+
+        private void textBox_TextChanged(object sender, EventArgs e)
+        {
+            if(textBox.Text != "")
+            {
+                button.Enabled = true;
+            }
+            else
+            {
+                button.Enabled = false;
+            }
+        }
+    }
+}
